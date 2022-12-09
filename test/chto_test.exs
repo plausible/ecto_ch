cd1eac21988589eee55c5840f5c1665959635a75
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-12-09 14:43:06 +0700

    break up tests

--- test/chto_test.exs
+++ test/chto_test.exs
@@ -3,32 +3,24 @@ defmodule ChtoTest do
   import Ecto.Query
   alias Ecto.Adapters.ClickHouse.Connection, as: SQL
 
-  defp to_sql(query, opts \\ []) do
-    {query, _params, _key} = Ecto.Query.Planner.plan(query, :all, Ecto.Adapters.ClickHouse)
-    {query, _} = Ecto.Query.Planner.normalize(query, :all, Ecto.Adapters.ClickHouse, _counter = 0)
-
-    query
-    |> tap(fn query ->
-      if opts = opts[:inspect] do
-        IO.inspect(Map.from_struct(query), opts)
-      end
-    end)
-    |> SQL.all()
-    |> IO.iodata_to_binary()
-  end
-
-  describe "SQL" do
-    test "all" do
+  describe "all" do
+    test "select one column" do
       query = select("events", [e], e.name)
       # TODO drop AS?
-      assert to_sql(query) == ~s[SELECT e0."name" FROM "events" AS e0]
+      assert all(query) == ~s[SELECT e0."name" FROM "events" AS e0]
+    end
 
+    test "select two columns" do
       query = select("events", [e], map(e, [:name, :user_id]))
-      assert to_sql(query) == ~s[SELECT e0."name",e0."user_id" FROM "events" AS e0]
+      assert all(query) == ~s[SELECT e0."name",e0."user_id" FROM "events" AS e0]
+    end
 
+    test "limit" do
       query = "events" |> select([e], e.name) |> limit(1)
-      assert to_sql(query) == ~s[SELECT e0."name" FROM "events" AS e0 LIMIT 1]
+      assert all(query) == ~s[SELECT e0."name" FROM "events" AS e0 LIMIT 1]
+    end
 
+    test "where with typed param" do
       name = "John"
       min_user_id = 10
 
@@ -38,16 +30,34 @@ defmodule ChtoTest do
         |> where([e], e.user_id > type(^min_user_id, :integer))
         |> select([e], e.user_id)
 
-      assert to_sql(query) ==
+      assert all(query) ==
                ~s[SELECT e0."user_id" FROM "events" AS e0 WHERE (e0."name" = {$0:String}) AND (e0."user_id" > {$1:Int64})]
+    end
+
+    test "where with fragment" do
+      name = "John"
 
       query =
         "events"
         |> where([e], fragment("name = ?", type(^name, :string)))
         |> select([e], e.user_id)
 
-      assert to_sql(query) ==
+      assert all(query) ==
                ~s[SELECT e0."user_id" FROM "events" AS e0 WHERE (name = {$0:String})]
     end
+  end
+
+  defp all(query) do
+    to_sql(query, fn query -> SQL.all(query) end)
+  end
+
+  defp to_sql(query, f) do
+    {query, _params, _key} = Ecto.Query.Planner.plan(query, :all, Ecto.Adapters.ClickHouse)
+    {query, _} = Ecto.Query.Planner.normalize(query, :all, Ecto.Adapters.ClickHouse, _counter = 0)
+    query |> f.() |> IO.iodata_to_binary()
+  end
+
+  def i(query) do
+    IO.inspect(Map.from_struct(query), limit: :infinity)
   end
 end
