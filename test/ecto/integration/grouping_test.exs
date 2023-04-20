5ce8599d9dc04ee132f433774b409462df0c7cde
Dario Heinisch <dario.heinisch@gmail.com>
2023-04-20 16:18:46 +0200

    Add test for showing grouping issue

---
+++ test/ecto/integration/grouping_test.exs
@@ -0,0 +1,29 @@
+defmodule Ecto.Integration.GroupingTest do
+  use Ecto.Integration.Case
+  import Ecto.Query
+
+  alias Ecto.Integration.TestRepo
+  alias Ecto.Integration.Post
+
+  test "Aggregating already known key" do
+    TestRepo.insert!(%Post{title: "1", counter: 1, public: true})
+    TestRepo.insert!(%Post{title: "2", counter: 2, public: true})
+    TestRepo.insert!(%Post{title: "3", counter: 3, public: true})
+    TestRepo.insert!(%Post{title: "4", counter: 4, public: true})
+
+    data =
+      TestRepo.all(
+        from(
+          p in Post,
+          select: %{
+            counter: fragment("modulo(?, 2) == 0", p.counter),
+            total_counter: sum(p.counter)
+          },
+          group_by: fragment("counter")
+        )
+      )
+
+    assert Enum.sort(data) ==
+             Enum.sort([%{counter: 0, total_counter: 4}, %{counter: 1, total_counter: 6}])
+  end
+end
