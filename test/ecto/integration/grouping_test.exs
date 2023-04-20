28ed45c83a81e0bf09a988c030df9be7505c6dfd
Dario Heinisch <dario.heinisch@gmail.com>
2023-04-20 16:24:51 +0200

    Add another test

--- test/ecto/integration/grouping_test.exs
+++ test/ecto/integration/grouping_test.exs
@@ -5,7 +5,7 @@ defmodule Ecto.Integration.GroupingTest do
   alias Ecto.Integration.TestRepo
   alias Ecto.Integration.Post
 
-  test "Aggregating already known key" do
+  test "Grouping on already known field" do
     TestRepo.insert!(%Post{title: "1", counter: 1, public: true})
     TestRepo.insert!(%Post{title: "2", counter: 2, public: true})
     TestRepo.insert!(%Post{title: "3", counter: 3, public: true})
@@ -25,5 +25,27 @@ defmodule Ecto.Integration.GroupingTest do
 
     assert Enum.sort(data) ==
              Enum.sort([%{counter: 0, total_counter: 4}, %{counter: 1, total_counter: 6}])
+  end
+
+  test "Grouping on new selected field" do
+    TestRepo.insert!(%Post{title: "1", counter: 1, public: true})
+    TestRepo.insert!(%Post{title: "2", counter: 2, public: true})
+    TestRepo.insert!(%Post{title: "3", counter: 3, public: true})
+    TestRepo.insert!(%Post{title: "4", counter: 4, public: true})
+
+    data =
+      TestRepo.all(
+        from(
+          p in Post,
+          select: %{
+            even: fragment("modulo(?, 2) == 0", p.counter),
+            total_counter: sum(p.counter)
+          },
+          group_by: fragment("even")
+        )
+      )
+
+    assert Enum.sort(data) ==
+             Enum.sort([%{even: 0, total_counter: 4}, %{even: 1, total_counter: 6}])
   end
 end
