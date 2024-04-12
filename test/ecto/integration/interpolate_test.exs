fac5f6f16418ecf459c6a1cc897d4e3b5a8cf757
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-04-12 20:36:17 +0800

    start adding proper tests

--- test/ecto/integration/interpolate_test.exs
+++ test/ecto/integration/interpolate_test.exs
@@ -8,19 +8,23 @@ defmodule Ecto.Integration.InterpolateTest do
     %{sql: sql, rows: TestRepo.query!(sql, _no_params = []).rows}
   end
 
-  describe "to_sql and exec" do
+  describe "interpolate and exec" do
+    # https://clickhouse.com/docs/en/sql-reference/data-types/int-uint
     test "with integers" do
       assert all(
                from n in fragment("numbers(3)"),
-                 where: n.number > ^1 and ^1 > 0,
-                 having: 5 > ^4,
-                 limit: ^200,
+                 where: n.number > ^1 and ^18_446_744_073_709_551_615 > 0,
+                 having: 5 > ^(-9_223_372_036_854_775_808),
+                 limit: ^2_147_483_647,
                  select: n.number
              ) == %{
                sql:
-                 ~s{SELECT f0."number" FROM numbers(3) AS f0 WHERE ((f0."number" > 1) AND (1 > 0)) HAVING (5 > 4) LIMIT 200},
+                 ~s{SELECT f0."number" FROM numbers(3) AS f0 WHERE ((f0."number" > 1) AND (18446744073709551615 > 0)) HAVING (5 > -9223372036854775808) LIMIT 2147483647},
                rows: [[2]]
              }
     end
+
+    # https://clickhouse.com/docs/en/sql-reference/data-types/float
+    test "with floats"
   end
 end
