1118ea369170a2f6bc6b0299bbb6ace094cab13e
Andy LeClair <andyleclair@gmail.com>
2024-10-15 01:19:41 -0400

    add union_distinct (#204)

--- lib/ecto/adapters/clickhouse/connection.ex
+++ lib/ecto/adapters/clickhouse/connection.ex
@@ -560,8 +560,9 @@ defmodule Ecto.Adapters.ClickHouse.Connection do
     Enum.map(combinations, &combination(&1, params))
   end
 
-  defp combination({:union, query}, params), do: [" UNION (", all(query, params), ?)]
+  defp combination({:union, query}, params), do: [" UNION DISTINCT (", all(query, params), ?)]
   defp combination({:union_all, query}, params), do: [" UNION ALL (", all(query, params), ?)]
+
   defp combination({:except, query}, params), do: [" EXCEPT (", all(query, params), ?)]
   defp combination({:intersect, query}, params), do: [" INTERSECT (", all(query, params), ?)]
 
