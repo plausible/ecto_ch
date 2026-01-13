ca5ea51647d9df69960fb5c9aaf061e5b4c537d0
ruslandoga <ruslandoga+gh@icloud.com>
2026-01-13 18:28:09 +0300

    update deps and allow Ch v0.7.x

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule EctoCh.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ch, "~> 0.5.0 or ~> 0.6.0"},
+      {:ch, "~> 0.5.0 or ~> 0.6.0 or ~> 0.7.0"},
       {:ecto_sql, "~> 3.13.0"},
       {:benchee, "~> 1.1", only: :bench},
       {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
